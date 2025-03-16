a5b7c8c7012ec45e3e27c2ec6ebb8007381d15e3
DevNexus Tec. <160817446+Okinda-joseph@users.noreply.github.com>
2025-03-16 15:10:22 +0300

    updates the file

---
+++ src/Verocart/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>Skeleton</h1>
+        <nav>
+          <li>Home</li>
+          <li>About</li>
+          <li>Service</li>
+          <li>Contact</li>
+        </nav>
+      </header>
+      <hr />
+    </>
+  );
+}
+
+export default Header;
